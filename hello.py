0de2fecab5563cb5a1ca688869bfe5c630cb07f0
yupenglei <yupengniaosanshi@gmail.com>
2023-08-28 00:38:38 +0800

    添加可变路由

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -6,3 +7,18 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, world</p>"
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User {escape(username)}'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post {post_id}'
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath {subpath}'
