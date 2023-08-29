b2443be7fa5234bfb0355c23125bf408dc4285b6
yupenglei <yupengniaosanshi@gmail.com>
2023-08-29 19:57:02 +0800

    测试request、response对象，redirect、abort方法

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, abort
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -6,7 +6,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, world</h1>"
+    user_agent = request.headers.get('User-Agent')
+    return f'<h1>UA</h1><p>{user_agent}</p>'
+
+
+@app.route("/error")
+def error():
+    # return "<h1>Bad Request</h1>", 400
+    response = make_response('<h1>带cookie的文档</h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+
+@app.route('/redirect')
+def redirect_page():
+    return redirect('https://baidu.com')
+
+
+@app.route('/abort/<int:code>')
+def abort_page(code: int):
+    abort(code)
+    return '<h1>abort page</h1>'
 
 
 @app.route('/user/<username>')
