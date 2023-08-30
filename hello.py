4d2258992c4eb85ddba55fe7da7076747c2206b0
yupenglei <yupengniaosanshi@gmail.com>
2023-08-30 11:11:22 +0800

    抽取bootstrap基模板；添加错误页

--- hello.py
+++ hello.py
@@ -52,3 +52,13 @@ def show_post(post_id):
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
     return f'Subpath {subpath}'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
