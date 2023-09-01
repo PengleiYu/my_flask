f17b90c83543dabcdfce7f6de59b91ac77ec92de
yupenglei <yupengniaosanshi@gmail.com>
2023-09-01 10:33:39 +0800

    添加db；视图函数使用db

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import os
+from flask_sqlalchemy import SQLAlchemy
 
 from flask import Flask, make_response, redirect, abort, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
@@ -7,10 +9,36 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "db.sqlite3")}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db: SQLAlchemy = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self) -> str:
+        return f'<Role {self.name}>'
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self) -> str:
+        return f'<User {self.username}>'
 
 
 class NameForm(FlaskForm):
@@ -22,14 +50,23 @@ class NameForm(FlaskForm):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
+        user = User.query.filter_by(username=form.name.data).first()
+        print(user)
         new_name = form.name.data
-        if old_name is not None and old_name != new_name:
-            flash('已修改name!')
+        if user is None:
+            user = User(username=new_name)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+
         session['name'] = new_name
         return redirect(url_for('index'))
     name = session.get('name')
-    return render_template("index.html", current_time=datetime.utcnow(), form=form, name=name)
+    known = session.get('known', False)
+    return render_template("index.html", current_time=datetime.utcnow(),
+                           form=form, name=name, known=known)
 
 
 @app.route("/list")
