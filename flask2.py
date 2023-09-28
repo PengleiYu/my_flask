cb0a3875a73cf7c28b0ed7f1f4e3c9bda82f0075
yupenglei <yupengniaosanshi@gmail.com>
2023-09-29 04:16:42 +0800

    添加单元测试

--- flask2.py
+++ flask2.py
@@ -12,3 +12,10 @@ migrate = Migrate(app, db)
 def make_shell_context():
     # todo 目前第一次部署时必须手动执行db创建过程
     return dict(db=db, User=User, Role=Role)
+
+
+@app.cli.command()
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
