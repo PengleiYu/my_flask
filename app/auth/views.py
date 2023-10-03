c0f246613738ad22e408d9c9ad6098662e3c9571
yupenglei <yupengniaosanshi@gmail.com>
2023-10-03 11:22:05 +0800

    实现功能：全局拦截未验证用户、重发验证邮件

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,11 +3,55 @@ from datetime import datetime
 from flask import render_template, request, url_for, redirect, flash
 from flask_login import login_required, login_user, logout_user, current_user
 
-from app.models import User
+from app import db
 from app.email import send_email
+from app.models import User
 from . import auth
 from .forms import LoginForm, RegistrationForm
-from app import db
+
+
+# 全局拦截
+@auth.before_app_request
+def before_request():
+    user: User = current_user
+    if user.is_authenticated \
+            and not user.confirmed \
+            and request.endpoint \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    user: User = current_user
+    if user.is_anonymous or user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html', user=user, current_time=datetime.utcnow())
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    # 根据email查询用户，查到则记录登录状态并重定向到之前页面
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next_url = request.args.get('next')
+            if next_url is None or not next_url.startswith("/"):
+                next_url = url_for('main.index')
+            return redirect(next_url)
+        flash('非法的用户名或密码')
+    return render_template('auth/login.html', form=form, current_time=datetime.utcnow())
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('您已登出')
+    return redirect(url_for('main.index'))
 
 
 @auth.route('/register', methods=['GET', 'POST'])
@@ -43,27 +87,13 @@ def confirm(token: str):
     return redirect(url_for('main.index'))
 
 
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
-    # 根据email查询用户，查到则记录登录状态并重定向到之前页面
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is not None and user.verify_password(form.password.data):
-            login_user(user, form.remember_me.data)
-            next_url = request.args.get('next')
-            if next_url is None or not next_url.startswith("/"):
-                next_url = url_for('main.index')
-            return redirect(next_url)
-        flash('非法的用户名或密码')
-    return render_template('auth/login.html', form=form, current_time=datetime.utcnow())
-
-
-@auth.route('/logout')
+@auth.route('/confirm')
 @login_required
-def logout():
-    logout_user()
-    flash('您已登出')
+def resend_confirm():
+    user: User = current_user
+    token = user.generate_confirmation_token()
+    send_email(user.email, '确认邮件地址', 'auth/email/confirm', user=user, token=token)
+    flash('新的确认邮件已经发送到你的邮箱')
     return redirect(url_for('main.index'))
 
 
